7d8ed651fb866cf4f8d163bf6a7f7d9d779dd4b9
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-12-03 10:42:52 +0300

    request

--- MachineLearningimageRecognition/MachineLearningimageRecognition/ViewController.swift
+++ MachineLearningimageRecognition/MachineLearningimageRecognition/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var resutLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    
+    var chosenImage = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +32,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true)
+        
+        if let ciImage = CIImage(image: imageView.image!) {
+            chosenImage = ciImage
+        }
+        
+        recognizeImage(image: chosenImage)
+        
+        
+    }
+    
+    func recognizeImage(image: CIImage){
+        
+        //request
+        //handler
+        
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+            let request = VNCoreMLRequest(model: model) { vnrequest, error in
+                if let results = vnrequest.results as? [VNClassificationObservation]{
+                    if results.count > 0 {
+                        let topResult = results.first
+                        
+                        DispatchQueue .main.async {
+                            let confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            self.resutLabel.text = String(confidenceLevel)+"% it's " + String(topResult!.identifier)
+                        }
+                    }
+                }
+        
+            }
+        }
+        
     }
     
 
