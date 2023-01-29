29ffbb87590e20523f8e18454962daa0396872ae
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-01-29 18:51:36 +0300

    CryptoApp getData

--- CryptoApp/CryptoApp/View/ViewController.swift
+++ CryptoApp/CryptoApp/View/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    private var cryptoListViewModel : CryptoListViewModel!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -18,24 +19,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
         
+        getData()
         
+    }
+    
+    
+    func getData(){
         let url = "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json"
         
         WebService().downloadCurrencies(url: URL(string: url)!) { cryptos in
             if let cryptos = cryptos {
-                print(cryptos)
+                
+                self.cryptoListViewModel = CryptoListViewModel(cryptoCurrencyList: cryptos)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             }
         }
-        
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.cryptoListViewModel == nil ? 0 : self.cryptoListViewModel.numberOfRowsInSection()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        
+        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
+        
+        cell.priceText.text = cryptoViewModel.price
+        cell.currencyText.text = cryptoViewModel.name
         
         return cell
     }
