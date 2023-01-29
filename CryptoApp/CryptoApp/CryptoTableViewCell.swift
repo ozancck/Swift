547f7c59700b5f3a2d0f83eccad581054c150250
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-01-29 15:54:23 +0300

    CryptoApp
    TabelView and Cell

---
+++ CryptoApp/CryptoApp/CryptoTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoApp
+//
+//  Created by Ozan Çiçek on 29.01.2023.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+    @IBOutlet weak var currencyText: UILabel!
+    
+    @IBOutlet weak var priceText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CryptoApp/CryptoApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by Ozan Çiçek on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        
+        return cell
+    }
+
+ 
+}
+
