7c50b6df3d3ba84196a7b1536ce7e11ae2c0ef96
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-01-29 16:12:31 +0300

    CryptoApp Model

---
+++ CryptoApp/CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by Ozan Çiçek on 29.01.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable {
+    let currency: String
+    let proce: String
+}
