517e71d88f205dd5cce295c13eaed55f317e77a7
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-01-31 00:05:45 +0300

    CryptoAppV2
    View functions

--- CryptoAppV2/CryptoAppV2/View.swift
+++ CryptoAppV2/CryptoAppV2/View.swift
@@ -11,7 +11,68 @@ import UIKit
 // Class, protocol
 
 protocol AnyView {
+    var presenter: AnyPresenter? { get set }
+
+    func update(with cryptos: [Crypto])
+    func update(with error: String)
 }
 
-class CryptoView: UIViewController, AnyView {
+class CryptoView: UIViewController, AnyView, UITableViewDataSource, UITableViewDelegate {
+    var presenter: AnyPresenter?
+    var cryptos : [Crypto] = []
+    
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading ..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.textAlignment = .center
+
+        return label
+
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        tableView.frame = view.bounds
+        messageLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 25, width: 200, height: 50)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func update(with cryptos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+
+    func update(with error: String) {
+        <#code#>
+    }
 }
