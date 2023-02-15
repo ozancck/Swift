ce737a7fedf5caeb069180d332929e8db6b4e6db
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-15 21:25:45 +0300

    CoreDataProject
    Design

---
+++ CoreDataProject/CoreDataProject/VCEkleDuzenle.swift
@@ -0,0 +1,40 @@
+//
+//  VCEkleDuzenle.swift
+//  CoreDataProject
+//
+//  Created by Ozan Çiçek on 15.02.2023.
+//
+
+import UIKit
+
+class VCEkleDuzenle: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    
+    @IBOutlet weak var tfSurname: UITextField!
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func btnVazgecTUI(_ sender: Any) {
+    }
+    @IBAction func btnKaydetTUI(_ sender: Any) {
+    }
+}
---
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Ozan Çiçek on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tvKisiListesi: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
